142bc7b8a749108ef5cbd00605ecaf8c8353653c
Yohan <yohan.boucher.yb@gmail.com>
2021-11-20 19:46:47 +0100

    Fix

--- src/Entity/DifficulteQuestion.php
+++ src/Entity/DifficulteQuestion.php
@@ -30,9 +30,15 @@ class DifficulteQuestion
      */
     private $salles;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Question::class, mappedBy="difficulty")
+     */
+    private $questions;
+
     public function __construct()
     {
         $this->salles = new ArrayCollection();
+        $this->questions = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -82,6 +88,36 @@ class DifficulteQuestion
 
         return $this;
     }*/
+
+    /**
+     * @return Collection|Question[]
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): self
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions[] = $question;
+            $question->setDifficulty($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): self
+    {
+        if ($this->questions->removeElement($question)) {
+            // set the owning side to null (unless already changed)
+            if ($question->getDifficulty() === $this) {
+                $question->setDifficulty(null);
+            }
+        }
+
+        return $this;
+    }
 }
 
 //INSERT INTO difficulte_question VALUES (1, 'Facile'), (2, 'Normale'),(3, 'Difficile')
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -18,40 +18,145 @@ class Question
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @ORM\Column(type="integer")
      */
-    private $Theme;
+    private $category;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @ORM\Column(type="boolean")
      */
-    private $Difficulte;
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=DifficulteQuestion::class, inversedBy="questions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $difficulty;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $answer;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $incorrect0;
+
+    /**
+     * @ORM\Column(type="text")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $incorrect1;
+
+    /**
+     * @ORM\Column(type="text")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $incorrect2;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTheme(): ?string
+    public function getCategory(): ?int
     {
-        return $this->Theme;
+        return $this->category;
     }
 
-    public function setTheme(string $Theme): self
+    public function setCategory(int $category): self
     {
-        $this->Theme = $Theme;
+        $this->category = $category;
 
         return $this;
     }
 
-    public function getDifficulte(): ?string
+    public function getType(): ?bool
     {
-        return $this->Difficulte;
+        return $this->type;
     }
 
-    public function setDifficulte(string $Difficulte): self
+    public function setType(bool $type): self
     {
-        $this->Difficulte = $Difficulte;
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?DifficulteQuestion
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(?DifficulteQuestion $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getIncorrect0(): ?string
+    {
+        return $this->incorrect0;
+    }
+
+    public function setIncorrect0(string $incorrect0): self
+    {
+        $this->incorrect0 = $incorrect0;
+
+        return $this;
+    }
+
+    public function getIncorrect1(): ?string
+    {
+        return $this->incorrect1;
+    }
+
+    public function setIncorrect1(string $incorrect1): self
+    {
+        $this->incorrect1 = $incorrect1;
+
+        return $this;
+    }
+
+    public function getIncorrect2(): ?string
+    {
+        return $this->incorrect2;
+    }
+
+    public function setIncorrect2(string $incorrect2): self
+    {
+        $this->incorrect2 = $incorrect2;
 
         return $this;
     }
